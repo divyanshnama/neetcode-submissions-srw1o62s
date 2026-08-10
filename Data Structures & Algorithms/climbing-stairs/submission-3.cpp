b183987fd713ad3353870134f7245474e365d4a3
divyanshnama <104021285+divyanshnama@users.noreply.github.com>
2026-08-10 09:55:05 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        if(n<=2) return n;
+        int one =2,two = 1;
+
+        for(int i =3;i<=n;i++)
+        {
+            int curr = one+two;
+            two =one;
+            one =curr;
+        }
+        return one;
+    }
+};
